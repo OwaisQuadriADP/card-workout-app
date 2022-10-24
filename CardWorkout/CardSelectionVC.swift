46036b7f83f8002f9c85dc0bafbbfc27c1f48d36
Owais Quadri <owais.quadri@adp.com>
2022-10-24 16:26:00 -0400

    button formatting

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
-
+//variables
+    @IBOutlet var cardImageView: UIImageView!// IB outlet to gain access and change properties with code
+    @IBOutlet var buttons: [UIButton]!
+    let cornerRadius: CGFloat = 5
+    //viewdidload / appear
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        for button in buttons {
+            button.layer.cornerRadius = cornerRadius
+        }
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //other funcs
+    @IBAction func stopButtonTapped(_ sender: UIButton) { // IB Action to create functions / onClick
+        
     }
-    */
+    
 
 }
