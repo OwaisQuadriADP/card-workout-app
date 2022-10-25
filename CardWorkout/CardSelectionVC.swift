43c547ad8f99eb1e3c1e1beb4245d309f2be3838
Owais Quadri <owais.quadri@adp.com>
2022-10-25 12:00:28 -0400

    Refactor and Format

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
-//variables
+    
+    
+    // outlets / outlet collections
     @IBOutlet var cardImageView: UIImageView!// IB outlet to gain access and change properties with code
-    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var buttons: [UIButton]!// IB outlet collection so I dont need to init all the elements here that I want in an array anyway
+    
+    
+    // class variables
     let cornerRadius: CGFloat = 5
     var timer: Timer!
     var cards: [UIImage]  = Card.allValues
-    //viewdidload / appear / disappear
+    
+    
+    // view___
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,23 +30,31 @@ class CardSelectionVC: UIViewController {
         }
         startTimer()
     }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)//call supers on overrieds
         timer.invalidate()//make sure timer turns off
     }
-    //other funcs
+    
+    
+    // other funcs (helpers)
     func startTimer(){
         timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(showRandomCard), userInfo: nil, repeats: true)
     }
+    
     @objc func showRandomCard(){
         cardImageView.image = cards.randomElement() ?? UIImage(named:"AS")
     }
+    
+    
+    // BI Actions
     @IBAction func stopButtonTapped(_ sender: UIButton) { // IB Action to create functions / onClick
         timer.invalidate()
     }
+    
     @IBAction func restartButtonTapped(_ sender: UIButton) {
-        timer.invalidate()
-        startTimer()
+        timer.invalidate()//make sure to turn it off
+        startTimer()//then restart
     }
     
 
